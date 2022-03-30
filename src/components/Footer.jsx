bea1cd0daec3b907eb588ee48f4c918a3400c123
Zhang Kaiwei <kaiwei.zh@qq.com>
2022-03-30 16:53:30 +0800

    responsive design in home page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 import { Facebook, Instagram, Twitter, MailOutline, Phone, Room } from '@mui/icons-material'
 import styled from 'styled-components'
+import { mobile } from '../responsive'
 
 const Container = styled.div`
   display: flex;
+  ${mobile({ flexDirection: 'column' })}
 `
 const Left = styled.div`
   flex: 1;
@@ -35,6 +37,7 @@ const SocialIcon = styled.div`
 const Center = styled.div`
   flex: 1;
   padding: 20px;
+  ${mobile({ display: 'none' })}
 `
 
 const Title = styled.h3`
